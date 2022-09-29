9ec9f22995b3a8eea707b54e6d08c49aade14d24
Ryan Barouki <ryanbarouki@gmail.com>
2022-09-29 13:26:50 +0100

    clean up

--- src/tree_generator.rs
+++ src/tree_generator.rs
@@ -13,16 +13,16 @@ pub struct TreeGenerator {
 }
 
 impl TreeGenerator {
-    pub fn generate_tree(mut iterations: i32, g: f64) -> tree::Tree<i32> {
+    pub fn generate_tree(iterations: i32, g: f64) -> tree::Tree<i32> {
         let mut tree = tree::Tree::new(0);
         let samples: Vec<i32> = (0..100).collect();
         let weights: Vec<f64> = samples.iter().map(|x| g.powi(-(x+1))).collect();
         let mut choices  = random_choice().random_choice_f64(&samples, &weights, iterations.try_into().unwrap());
         choices.shuffle(&mut thread_rng());
-        println!("{:?}", choices);
         let mut node_queue: Queue<NodeId> = queue![];
         let mut current = tree.root;
         let mut count = 1;
+
         for num_children in choices {
             if *num_children == 0 {
                 continue;
